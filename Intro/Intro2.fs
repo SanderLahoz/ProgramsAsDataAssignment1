0b476fd3dc44e2a254c912cd47c6a9e68e89c60b
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-08-28 21:13:55 +0200

    Refactor code cleanup

--- Intro/Intro2.fs
+++ Intro/Intro2.fs
@@ -33,24 +33,6 @@ type aexpr =
     | Mul of aexpr * aexpr
     | Sub of aexpr * aexpr
 
-let e1: expr = expr.CstI 17
-
-let e2 = Prim("+", expr.CstI 3, expr.Var "a")
-
-let e3 = Prim("+", Prim("*", expr.Var "b", expr.CstI 9), expr.Var "a")
-
-// Should evaluate to false (5 + 7) == 10 ~> false
-let e4 = Prim("==", Prim("+", expr.CstI 5, expr.CstI 7), expr.CstI 10)
-
-// Should evaluate to true ("a" + 7) ~> (3 + 7) == 10 ~> true
-let e5 = Prim("==", Prim("+", expr.Var "a", expr.CstI 7), expr.CstI 10)
-let e6 = Prim("min", expr.CstI 5, expr.CstI 6)
-let e7 = Prim("max", expr.CstI 5, expr.CstI 6)
-
-// Arithmetic expressions
-let e8 = Sub(Var "v", Add(Var "w", Var "z"))
-let e9 = Mul(CstI 2, Sub(Var "v", Add(Var "w", Var "z")))
-let e10 = Add(Add(Add(Var "x", Var "y"), Var "z"), Var "v")
 
 (* Evaluation within an environment *)
 
@@ -72,6 +54,7 @@ let rec eval (e: expr) (env: (string * int) list) : int =
         else
             eval e2 env
     | Prim("==", e1, e2) -> if (eval e1 env) = (eval e2 env) then 1 else 0
+
 
 // Formatting arithmetic expressions
 let rec fmt ae : string =
@@ -97,11 +80,36 @@ let rec eval' e (env: (string * int) list) : int =
         | "min" -> if i1 < i2 then i1 else i2
         | "max" -> if i1 > i2 then i1 else i2
         | "==" -> if i1 = i2 then 1 else 0
+        | _ -> failwith "unexpected operator"
+
     | If(e1, e2, e3) ->
         if (eval' e1 env) > 0 then
             (eval' e2 env)
         else
             (eval' e3 env)
+
+
+// Examples and testing
+
+let e1: expr = expr.CstI 17
+
+let e2 = Prim("+", expr.CstI 3, expr.Var "a")
+
+let e3 = Prim("+", Prim("*", expr.Var "b", expr.CstI 9), expr.Var "a")
+
+// Should evaluate to false (5 + 7) == 10 ~> false
+let e4 = Prim("==", Prim("+", expr.CstI 5, expr.CstI 7), expr.CstI 10)
+
+// Should evaluate to true ("a" + 7) ~> (3 + 7) == 10 ~> true
+let e5 = Prim("==", Prim("+", expr.Var "a", expr.CstI 7), expr.CstI 10)
+let e6 = Prim("min", expr.CstI 5, expr.CstI 6)
+let e7 = Prim("max", expr.CstI 5, expr.CstI 6)
+
+// Arithmetic expressions
+let e8 = Sub(Var "v", Add(Var "w", Var "z"))
+let e9 = Mul(CstI 2, Sub(Var "v", Add(Var "w", Var "z")))
+let e10 = Add(Add(Add(Var "x", Var "y"), Var "z"), Var "v")
+
 
 let e1v = eval e1 env
 let e2v1 = eval e2 env
