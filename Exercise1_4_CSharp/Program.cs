70c02d95f92ffa48266171dea412804026d77866
Maryna Lvova <mlvo@itu.dk>
2026-08-29 18:54:04 +0200

    Added 3 more expressions in abstract syntax 1.4 ii)

--- Exercise1_4_CSharp/Program.cs
+++ Exercise1_4_CSharp/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using DefaultNamespace;
+
+Expr e = new Add(new CstI(17), new Var("z"));
+Console.WriteLine(e.ToString());
+
+Expr e1 = new Sub(new CstI(35), new CstI(5));
+Console.WriteLine(e1.ToString());
+
+Expr e2 = new Mul(new CstI(2), new Var("a"));
+Console.WriteLine(e2.ToString());
+
+Expr e3 = new Mul(new CstI(10), new CstI(2));
+Console.WriteLine(e3.ToString());
